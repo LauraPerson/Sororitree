0ee0ce2d59086cbf93eb29d6b828ec5d5dc3a17e
Alexis Lenoir <alexis59495@gmail.com>
2020-05-23 16:09:02 -0300

    Start Front-end profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,4 @@ class User < ApplicationRecord
   has_many :matching_profiles
   has_many :messages
   validates :nickname, presence: true, uniqueness: { case_sensitive: false }
-  validates :helper
 end
