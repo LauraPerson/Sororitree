2f7a3a573b05375452634a38014295f771070f80
Andressa DAgostini <ad.dagostini@gmail.com>
2020-05-23 15:44:47 -0300

    Put post new into index method and made the view posts/index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,19 @@ class PostsController < ApplicationController
   def index
     @posts = policy_scope(Post)
     authorize @posts
+    @post = Post.new()
+    authorize @post
   end
 
   def create
     @post = Post.new(params_posts)
+    @post.theme = Theme.first
     authorize @post
-    if @post.save
-      # redirect_to
+    @post.user = current_user
+    if @post.save!
+      redirect_to posts_path
     else
-      render '/posts'
+      render :index
     end
   end
 
