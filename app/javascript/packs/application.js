972ffb34c53b3321c34ff1a14b7ba8e1f0798ae0
Amanda Juliao <amanda.juliao@hotmail.com>
2020-05-24 19:58:54 -0300

    created navbar need to add logic to avatar picture

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,12 @@
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+
+document.addEventListener('turbolinks:load', () => {
+  initUpdateNavbarOnScroll();
+});
