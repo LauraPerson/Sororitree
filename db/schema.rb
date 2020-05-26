0977b36f50532de5632d7361c2f2659a7b403a60
Andressa DAgostini <ad.dagostini@gmail.com>
2020-05-26 15:47:29 -0300

    Added birth date to User to make the age filter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_183017) do
+ActiveRecord::Schema.define(version: 2020_05_26_184709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_183017) do
     t.text "address"
     t.float "latitude"
     t.float "longitude"
+    t.date "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
