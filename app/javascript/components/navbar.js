ddf7e59eee55316b7c7ef671ed9a41e8e0e8d7cd
Alexis Lenoir <alexis59495@gmail.com>
2020-05-26 00:03:03 -0300

    Correct navbar and banner bugs

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,6 +3,7 @@ const transparentNabarHome = () => {
   if (banner) {
     const navbar = document.querySelector('.navbar-sororitree');
     navbar.classList.add('nav-transparent');
+    navbar.classList.add('fixed-top');
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.remove('nav-transparent');
