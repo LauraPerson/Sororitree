7c0fbaf62e977573146f4f089338f8c13b4a5d04
Alexis Lenoir <alexis59495@gmail.com>
2020-05-27 00:35:47 -0300

    add icon for the boolean Choice Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def icon_helper(name, size = 2.5)
+    html = "<i class='far fa-#{name}' "
+    html += "style='font-size:#{size}em' "
+    html += "></i>"
+    html.html_safe
+  end
 end
