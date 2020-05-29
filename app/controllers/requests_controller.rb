5469ea25f97f63ac17df163b73e01ea940d74346
Andressa DAgostini <ad.dagostini@gmail.com>
2020-05-28 22:05:52 -0300

    No PG Search, Added filter of helpers and by age

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -10,7 +10,7 @@ class RequestsController < ApplicationController
     @request = Request.new(params_request)
     authorize @request
     if @request.save
-      # redirect_to matching_profile
+      #redirect_to request_matching_profile_path
     else
       render :new
     end
@@ -18,7 +18,15 @@ class RequestsController < ApplicationController
 
   private
 
+  def search(age_min, age_max)
+    helpers = User.where(helper: true)
+    year_min = DateTime.now().year - age_min
+    year_max = DateTime.now().year - age_max
+    helpers = helpers.where("extract(year from birth_date) >= ?", year_min)
+    helpers = helpers.where("extract(year from birth_date) >= ?", year_max)
+  end
+
   def params_request
-    params.require(:request).permit(:age, :distance, :theme_id, :user_id)
+    params.require(:request).permit(:age_min, :age_max, :distance, :theme_id, :user_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_05_27_010835) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
