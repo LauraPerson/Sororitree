e2eacbc490a334b1ee459864273f4d4b3cfbbfeb
Laura Person <lauraperson29800@gmail.com>
2020-06-03 22:17:07 -0300

    add an anchor in messages controller for the chat not to get back on the top

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,15 +1,7 @@
 import consumer from "./consumer";
 
 const initChatroomCable = () => {
-  const messagesContainer = document.getElementById('chat-container');
-//   messagesContainer.scrollblabla = messagesContainer.scrollHeight;
-
-//   if (firstTime) {
-//   messagescontainer.scrollTop = messagesContainer.scrollHeight;
-//   firstTime = false;
-// } else if (messagesContainer.scrollTop + messagesContainer.clientHeight === messagesContainer.scrollHeight) {
-//   messagesContainer.scrollTop = messagesontainer.scrollHeight;
-// }
+const messagesContainer = document.getElementById('chat-container');
 console.log("broadcast_received")
 
   if (messagesContainer) {
