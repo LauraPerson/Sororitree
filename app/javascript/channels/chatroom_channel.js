5630f3f77fb733ca4eba11ae0e03b63bdd5b70b0
Laura Person <lauraperson29800@gmail.com>
2020-06-04 14:29:21 -0300

    add comments lines to chatrrom_channel.js, branch not finished

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -2,6 +2,10 @@ import consumer from "./consumer";
 
 const initChatroomCable = () => {
 const messagesContainer = document.getElementById('chat-container');
+  // if (messagesContainer.scrollTop + messagesContainer.clientHeight === messagesContainer.scrollHeight) {
+  //   messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  // }
+
 console.log("broadcast_received")
 
   if (messagesContainer) {
