82ae5821038302bdb1a038b2921c09f17da49e66
Laura Person <lauraperson29800@gmail.com>
2020-06-11 22:30:01 -0300

    add style on home page, change first image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     authorize @posts
     @random_posts = []
     (1..3).each do
-      random = rand(Post.count)
+      random = rand(1..Post.count).to_i
       @random_posts << Post.find(random)
     end
   end
